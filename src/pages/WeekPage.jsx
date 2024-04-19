64425133f85b9b6bd1bbf45d7863cd0e8b87d13d
NarciSource <jwch11@gmail.com>
2024-04-19 23:58:10 +0900

    feat: 오늘 요일이 순서 처음으로 오도록 추가

--- src/pages/WeekPage.jsx
+++ src/pages/WeekPage.jsx
@@ -3,14 +3,15 @@ import RatingsItem from "../components/RatingsItem";
 import { useSelector } from "react-redux";
 
 function WeekPage() {
-    const daysOfWeek = ["월", "화", "수", "목", "금", "토", "일"];
+    const daysOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
+    const todayOfWeek = new Date().getDay();
     const ratingsOfWeek = useSelector((state) => state.ratings.ratingsOfWeek);
 
     return (
         <div>
             <h1>일주일 컨디션</h1>
             <ul>
-                {daysOfWeek.map((dayOfWeek, idx) => (
+                {[...daysOfWeek.slice(todayOfWeek), ...daysOfWeek.slice(0, todayOfWeek)].map((dayOfWeek, idx) => (
                     <li key={idx}>
                         <RatingsItem dayOfWeek={dayOfWeek} ratings={ratingsOfWeek[dayOfWeek]} />
                         <div>

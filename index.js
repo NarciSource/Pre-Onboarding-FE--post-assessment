356b5842f1389d114e0279615053ea64bde2029a
NarciSource <jwch11@gmail.com>
2024-04-29 17:30:00 +0900

    feat: GET 구현

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 import http from "http";
 import dotenv from "dotenv";
+import fs from "fs";
 
 dotenv.config();
 
 http.createServer((req, res) => {
-    res.end("Hello");
+    const dbpath = "./db.json";
+    const pathname = "biological-rhythm";
+
+    switch (req.method) {
+        case "GET":
+            return fs.readFile(dbpath, (err, data) => {
+                if (err) {
+                    throw err;
+                }
+
+                const jsonData = JSON.parse(data);
+                const foundList = jsonData[pathname];
+
+                res.setHeader("Content-Type", "application/json");
+                res.end(JSON.stringify(foundList));
+            });
+
+        case "POST":
+            return;
+
+        default:
+            return;
+    }
 }).listen(process.env.PORT);

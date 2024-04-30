55955beec8cbe80486bacb54d683a21e63db0cdc
NarciSource <jwch11@gmail.com>
2024-04-30 19:23:00 +0900

    feat: POST, OPTIONS, upsert 구현

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,23 @@
 import { Router } from "express";
 import fs from "fs";
+import crypto from "crypto";
+
 var router = Router();
+
+function upsert(list, key, value, target) {
+    const index = list.findIndex((item) => item[key] === value);
+    const changedList = JSON.parse(JSON.stringify(list));
+
+    if (index !== -1) {
+        changedList[index] = { ...list[index], ...target };
+    } else {
+        changedList.push({
+            id: crypto.randomBytes(1).toString("hex"),
+            ...target,
+        });
+    }
+    return changedList;
+}
 
 router.get("/:pathname", function (req, res, next) {
     const { pathname } = req.params;
@@ -16,6 +33,34 @@ router.get("/:pathname", function (req, res, next) {
         res.setHeader("Content-Type", "application/json");
         res.json(found_list);
     });
+});
+
+router.post("/:pathname", function (req, res, next) {
+    const { pathname } = req.params;
+    const key = "date";
+
+    var target = req.body;
+
+    return fs.readFile("./db.json", (err, data) => {
+        if (err) {
+            throw err;
+        }
+
+        const json_data = JSON.parse(data);
+        const found_list = json_data[pathname];
+
+        const changed_list = upsert(found_list, key, target[key], target);
+
+        json_data[pathname] = changed_list;
+        fs.writeFile("./db.json", JSON.stringify(json_data), () => {});
+
+        res.end("successful");
+    });
+});
+
+router.options("/:pathname", function (req, res, next) {
+    res.writeHead(204); // No Content
+    res.end();
 });
 
 export default router;

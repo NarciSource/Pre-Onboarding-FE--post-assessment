182c993646f11a635dfc9c24e52b3497872b3a1e
NarciSource <jwch11@gmail.com>
2024-04-30 19:11:00 +0900

    feat: GET 구현

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 import { Router } from "express";
+import fs from "fs";
 var router = Router();
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-    res.end("Express");
+router.get("/:pathname", function (req, res, next) {
+    const { pathname } = req.params;
+
+    fs.readFile("./db.json", (err, data) => {
+        if (err) {
+            throw err;
+        }
+
+        const json_data = JSON.parse(data);
+        const found_list = json_data[pathname];
+
+        res.setHeader("Content-Type", "application/json");
+        res.json(found_list);
+    });
 });
 
 export default router;
